1c9a27011fd2ba06a1ac7474092f748bb58edf19
lsy1998 <1945934866@qq.com>
2020-04-12 00:48:42 +0800

    获取文件数量

--- file.py
+++ file.py
@@ -5,6 +5,7 @@ cursor = db.cursor()
 path = r'C:\Users\hasee\Desktop\dicomPic'
 fileList = os.listdir(path)
 print(fileList)
+
 for j in range(len(fileList)-1):
     for k in range(len(fileList)-1):
         if int(fileList[k]) > int(fileList[k+1]):
@@ -12,8 +13,23 @@ for j in range(len(fileList)-1):
             fileList[k] = int(fileList[k+1])
             fileList[k+1] = t
 print(fileList)
+
 for name in fileList:
     sql = f"""INSERT INTO dicompic(dicomPath) VALUES (\'{name}\')"""
     print(sql)
     result = cursor.execute(sql)
     db.commit()
+
+for name in fileList:
+    childPath = r'C:\Users\hasee\Desktop\dicomPic'
+    childPath = childPath + '\\'
+    childPath = childPath + str(name)
+    childFileList = os.listdir(childPath)
+    print(len(childFileList)-1)
+    print(childFileList)
+    childPath = ''
+    sql = f"""update dicompic set dicomLength = {len(childFileList)-1} where dicomPath = \'{str(name)}\'"""
+    print(sql)
+    result = cursor.execute(sql)
+    db.commit()
+
